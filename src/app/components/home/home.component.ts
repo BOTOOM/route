0492f17b1e16aaf8ef6520bf7550793fa267e328
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-04-30 00:20:29 -0500

    creacion de matrices y llenado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  tamano: number;
+  matrizQ = [];
+  matrizX = [];
+  matrizXT = [];
+  matrizclave = [];
+  Qon: boolean;
+  constructor() {
+    this.Qon = false;
+  }
 
   ngOnInit() {
+  }
+
+  crearMatriz() {
+    this.Qon = true;
+    this.matrizQ = [];
+    console.log(this.tamano);
+    for (let i = 0; i < this.tamano; i++) {
+      this.matrizQ[i] = new Array(this.tamano);
+      this.matrizX[i] = new Array(this.tamano);
+      this.matrizXT[i] = new Array(this.tamano);
+      this.matrizclave[i] = new Array(this.tamano);
+    }
+    for (let i = 0; i < this.tamano; i++) {
+      for (let j = 0; j < this.tamano; j++) {
+        this.matrizQ[i][j] = 0;
+      }
+    }
+    // console.log(this.matrizQ);
+  }
+
+  obtenerQ() {
+    console.log(this.matrizQ);
+    this.invertir();
+  }
+
+  invertir() {
+    for (let i = 0; i < this.tamano; i++) {
+      for (let j = 0; j < this.tamano; j++) {
+        this.matrizXT[j][i] = this.matrizQ[i][j];
+      }
+    }
+    console.log(this.matrizXT);
   }
 
 }
