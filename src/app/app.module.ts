2e1fd96b3f7a9270906273846b124fc82598dd1f
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-04-29 22:38:03 -0500

    nav y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule  } from '@angular/core';
+
+
+// rutas
+import {APP_ROUTING} from './app.routes';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule ,
+    RouterModule.forRoot(APP_ROUTING, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
