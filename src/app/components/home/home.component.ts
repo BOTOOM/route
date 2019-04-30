3f231e3e3b77cf34b850263b55bdeafd3bf17090
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-04-30 00:52:42 -0500

    multiplicacion de matrices

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,8 +13,12 @@ export class HomeComponent implements OnInit {
   matrizXT = [];
   matrizclave = [];
   Qon: boolean;
+  res: boolean;
+  resT: boolean;
   constructor() {
     this.Qon = false;
+    this.res = false;
+    this.resT = false;
   }
 
   ngOnInit() {
@@ -22,7 +26,12 @@ export class HomeComponent implements OnInit {
 
   crearMatriz() {
     this.Qon = true;
+    this.res = false;
+    this.resT = false;
     this.matrizQ = [];
+    this.matrizclave = [];
+    this.matrizX = [];
+    this.matrizXT = [];
     console.log(this.tamano);
     for (let i = 0; i < this.tamano; i++) {
       this.matrizQ[i] = new Array(this.tamano);
@@ -33,6 +42,9 @@ export class HomeComponent implements OnInit {
     for (let i = 0; i < this.tamano; i++) {
       for (let j = 0; j < this.tamano; j++) {
         this.matrizQ[i][j] = 0;
+        this.matrizX[i][j] = 0;
+        this.matrizXT[i][j] = 0;
+        this.matrizclave[i][j] = 0;
       }
     }
     // console.log(this.matrizQ);
@@ -40,16 +52,36 @@ export class HomeComponent implements OnInit {
 
   obtenerQ() {
     console.log(this.matrizQ);
-    this.invertir();
+    // this.invertir();
   }
 
-  invertir() {
+  obtenerClave() {
+    console.log(this.matrizclave);
+    // this.invertir();
+    this.clave_X_Q();
+  }
+
+  trapuesta() {
     for (let i = 0; i < this.tamano; i++) {
       for (let j = 0; j < this.tamano; j++) {
-        this.matrizXT[j][i] = this.matrizQ[i][j];
+        this.matrizX[j][i] = this.matrizXT[i][j];
       }
     }
     console.log(this.matrizXT);
+    this.resT = true;
+  }
+
+  clave_X_Q() {
+    for (let i = 0; i < this.tamano; i++) {
+      for (let j = 0; j < this.tamano; j++) {
+        for (let l = 0; l < this.tamano; l++) {
+          this.matrizXT[i][j] = this.matrizXT[i][j] +  (this.matrizclave[i][l] * this.matrizQ[l][j]);
+        }
+      }
+    }
+    console.log(this.matrizXT);
+    this.res = true;
+    this.trapuesta();
   }
 
 }
