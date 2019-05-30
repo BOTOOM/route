6e0680703f899319f33c6af80ef907669a71282c
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-05-29 19:04:39 -0500

    se recibe texto si se divide para su analisis

--- src/app/components/route-local/route-local.component.ts
+++ src/app/components/route-local/route-local.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouteLocalComponent implements OnInit {
 
+  modoIngresoSelect: string;
+  OS_select: string;
+  OS = ['Windows', 'Linux'];
+  modoIngreso = ['archivo de texto', 'texto plano'];
+  texto_plano: string;
+  separacion = [];
+
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  obtenerTraza() {
+    // console.log(this.OS_select);
+    this.separacion = this.texto_plano.split(/\n/);
+    console.log(this.separacion);
+    for (let i = 0; i < this.separacion.length; i++) {
+      // const trazaindi: string[] = separado[i].split(/\n/);
+      // separado[i] =  trazaindi as string[];
+      // console.log(this.separacion[i]);
+      this.separacion[i] = this.separacion[i].split(' ');
+    }
+    console.log(this.separacion);
   }
 
 }
